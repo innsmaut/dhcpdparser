1d366a670a48f760c17b8ecd12183d3524fd4a1a
innsmaut <co3_w4@mail.ru>
2016-07-09 20:25:06 +0300

    fast stable

--- index.php
+++ index.php
@@ -9,30 +9,39 @@
         <h1>TEST BODY</h1>
         <script>
             <?php
-            $i = 0;
-            $rawLog = fopen("C:/Users/innsmaut/Desktop/dhcpd1.txt", "r") or die ("Unable to open");
+
+            $rawLog = fopen("C:/Users/innsmaut/Desktop/dhcpd2.txt", "r") or die ("Unable to open");
+            $time = microtime(TRUE);
 
             while (!feof($rawLog)) {
-                $curStr = fgets($rawLog);
-                if ($curStr[35]=='i' && $curStr[40]=='a') {
-                    $length = ($curStr[57]==' ')?11:($curStr[58]==' ')?12:($curStr[59]==' ')?13:14;
-                    $curStr = trim(substr($curStr, 46, $length));
+
+                $curStr = substr(fgets($rawLog), 35);
+                $caser = 0;
+                if($curStr[0]=='i' && $curStr[5]=='a') { $caser = 1; 
+                } elseif ($curStr[2]=='n'){ $caser = 2; 
+                } elseif ($curStr[0]=='i' && $curStr[5]=='p') { $caser = 3;
+                } elseif ($curStr[4]=='o' && !($curStr[9]=='.' && $curStr[7]=='1') && !($curStr[10]=='2' && $curStr[8]=='9' && $curStr[7]=='1')) { $caser = 4;}
+
+                if ($caser == 1) {
+                    $curStr = trim(substr($curStr, 11));
                     $switchIp = $curStr;
                 }
-                if ($curStr[34]=='l' && $curStr[40]=='M') {
-                    $curStr = substr($curStr, 45, 17);
+
+                if ($caser == 2) {
+                    $curStr = trim(substr($curStr, 10));
                     $a = explode(':', $curStr);
                     for ($j=0; $j<5; $j++) { $a[$j] = (strlen($a[$j]) == 1)?'0'.$a[$j]:$a[$j]; }
                     $clientMac = trim($a[0].':'.$a[1].':'.$a[2].':'.$a[3].':'.$a[4].':'.$a[5]);
                 }
-                if ($curStr[35]=='i' && $curStr[40]=='p') {
-                    $curStr = trim(substr($curStr, 45, 2));
+
+                if ($caser == 3) {
+                    $curStr = trim(substr($curStr, 10));
                     $switchPort = $curStr;
                 }
 
-                if ($curStr[39]=='o' && !($curStr[44]=='.' && $curStr[42]=='1') && !($curStr[45]=='2' && $curStr[43]=='9' && $curStr[42]=='1')){
+                if ($caser == 4){
                     $to = strpos($curStr, ' to ');
-                    $ipadd = trim(substr($curStr, 42, $to-41));
+                    $ipadd = trim(substr($curStr, 7, $to-6));
                     $macadd = trim(substr($curStr, $to+4, 17));
                 }
 
@@ -62,6 +71,7 @@
                 echo "\n".$x.". ".$final[$macadd][0]."     p:".$final[$macadd][1]."    ".$macadd."    ".$final[$macadd][2]."<br>";
                 $x++;
             }
+            print_r(array('seconds' => microtime(TRUE) - $time));
         ?>
     </body>
 </html>
