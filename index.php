ae0d581709a7b9147a273d37986a7bd4339695f2
innsmaut <co3_w4@mail.ru>
2016-07-09 06:41:12 +0300

    g

--- index.php
+++ index.php
@@ -14,24 +14,52 @@
 
             while (!feof($rawLog)) {
                 $curStr = fgets($rawLog);
-                $curStr = substr($curStr, 42);
-                if ($curStr[0]=='8'){
-                    echo $curStr;
-                    $to = ($curStr[10]==' ')?10:($curStr[11]==' ')?11:12;
-                    $ipadd = substr($curStr, 0, $to);
-                    $macadd = substr($curStr, $to+4, 17);
-                    $result[$ipadd] = $macadd;
-                    
-                $i++;
+
+                if ($curStr[39]=='o' && !($curStr[44]=='.' && $curStr[42]=='1')){
+                    $to = strpos($curStr, ' to ');
+                    $ipadd = trim(substr($curStr, 42, $to-41));
+                    $macadd = trim(substr($curStr, $to+4, 17));
+                    $result[$macadd] = $ipadd;
+                }
+                if ($curStr[35]=='i' && $curStr[40]=='a') {
+                    $length = ($curStr[57]==' ')?11:($curStr[58]==' ')?12:($curStr[59]==' ')?13:14;
+                    $curStr = trim(substr($curStr, 46, $length));
+                    $switchIp[$i] = $curStr;
+                }
+                if ($curStr[34]=='l' && $curStr[40]=='M') {
+                    $curStr = substr($curStr, 45, 17);
+                    $a = explode(':', $curStr);
+                    for ($j=0; $j<5; $j++) { $a[$j] = (strlen($a[$j]) == 1)?'0'.$a[$j]:$a[$j]; }
+                    $clientMac[$i] = trim($a[0].':'.$a[1].':'.$a[2].':'.$a[3].':'.$a[4].':'.$a[5]);
+                    $i++;
+                }
+                if ($curStr[35]=='i' && $curStr[40]=='p') {
+                    $curStr = trim(substr($curStr, 45, 2));
+                    $switchPort[$i] = $curStr;
                 }
 
+
+
+
+            }
+            $p = 0;
+            for ($x=0; $x<$i; $x++){
+                foreach ($result as $macadd=>$ipadd){
+                    if ($macadd == $clientMac[$x]){
+                        $final[$p][0] = $switchIp[$x];
+                        $final[$p][1] = $switchPort[$x];
+                        $final[$p][2] = $clientMac[$x];
+                        $final[$p][3] = $ipadd;
+                        $p++;
+                    }
+                }
             }
             fclose($result);
             ?>
         </script>
         <?php
-            foreach ($result as $ipadd=>$macadd) {
-                echo $ipadd."  ".$macadd."<br>";
+            for ($x=0; $x<$p; $x++){
+                echo "\n".$x.". ".$final[$x][0]."     p:".$final[$x][1]."    ".$final[$x][2]."    ".$final[$x][3]."<br>";
             }
         ?>
     </body>
